8e48103da8e81d23eefcbd1884cbed3ab5a08b8f
lhashing <jamyangh@gmail.com>
2021-03-15 19:07:08 +0600

    ITW202_TODO_3

---
+++ TODO_3/app/src/main/java/edu/gcit/todo_3/MainActivity.java
@@ -0,0 +1,20 @@
+package edu.gcit.todo_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Receive(View view) {
+        Intent obj = new Intent(this,MainActivity2.class);
+        startActivity(obj);
+    }
+}

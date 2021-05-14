8d6b5a78bc1d04931d01fc8c167fb020a0289248
lhashing <jamyangh@gmail.com>
2021-05-14 16:43:04 +0600

    ITW202_TODO_18

---
+++ TODO_18/app/src/main/java/edu/gcit/todo_18/DetailActivity.java
@@ -0,0 +1,47 @@
+package edu.gcit.todo_18;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.bumptech.glide.Glide;
+
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        //Initialize the views
+        TextView sportsTitle = (TextView)findViewById(R.id.titleDetail);
+        ImageView sportsImage = (ImageView)findViewById(R.id.sportsImageDetail);
+
+        //Get the drawable
+        Drawable drawable = ContextCompat.getDrawable
+                (this,getIntent().getIntExtra(Sport.IMAGE_KEY, 0));
+
+        //Create a placeholder gray scrim while the image loads
+        GradientDrawable gradientDrawable = new GradientDrawable();
+        gradientDrawable.setColor(Color.GRAY);
+
+        //Make it the same size as the image
+        if(drawable!=null) {
+            gradientDrawable.setSize(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+        }
+
+        //Set the text from the Intent extra
+        sportsTitle.setText(getIntent().getStringExtra(Sport.TITLE_KEY));
+
+        //Load the image using the glide library and the Intent extra
+        Glide.with(this).load(getIntent().getIntExtra(Sport.IMAGE_KEY,0))
+                .placeholder(gradientDrawable).into(sportsImage);
+    }
+}
---
+++ TODO_18/app/src/main/java/edu/gcit/todo_18/Sport.java
@@ -0,0 +1,42 @@
+package edu.gcit.todo_18;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.DrawableRes;
+
+public class Sport {
+
+    //Member variables representing the title, image and information about the sport
+    private final String title;
+    private final String info;
+    private final int imageResource;
+
+    static final String TITLE_KEY = "Title";
+    static final String IMAGE_KEY = "Image Resource";
+
+    public Sport(String title, String info, int imageResource) {
+        this.title = title;
+        this.info = info;
+        this.imageResource = imageResource;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    public static Intent starter(Context context, String title, @DrawableRes int imageResId) {
+        Intent detailIntent = new Intent(context, DetailActivity.class);
+        detailIntent.putExtra(TITLE_KEY, title);
+        detailIntent.putExtra(IMAGE_KEY, imageResId);
+        return detailIntent;
+    }
+}

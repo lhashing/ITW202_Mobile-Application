4be2b4c785fd956a8a56ba3e36bac082c8f8a4a5
lhashing <jamyangh@gmail.com>
2021-05-27 12:56:18 +0600

    ITW202_TODO_22

---
+++ TODO_22/app/src/main/java/edu/gcit/todo_22/MainActivity.java
@@ -0,0 +1,72 @@
+package edu.gcit.todo_22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    // Variables for the search input field, and results TextViews.
+    private EditText mBookInput;
+    private TextView mTitleText;
+    private TextView mAuthorText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize all the view variables.
+        mBookInput = (EditText)findViewById(R.id.bookInput);
+        mTitleText = (TextView)findViewById(R.id.titleText);
+        mAuthorText = (TextView)findViewById(R.id.authorText);
+    }
+
+    public void searchBooks(View view) {
+        // Get the search string from the input field.
+        String queryString = mBookInput.getText().toString();
+
+        // Hide the keyboard when the button is pushed.
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if (inputManager != null ) {
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        new FetchBook(mTitleText, mAuthorText).execute(queryString);
+        mAuthorText.setText("");
+        mTitleText.setText(R.string.loading);
+
+        // Check the status of the network connection.
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connMgr != null) {
+            networkInfo = connMgr.getActiveNetworkInfo();
+        }
+
+        // If the network is active and the search field is not empty, start a FetchBook AsyncTask.
+        if (networkInfo != null && networkInfo.isConnected() && queryString.length() != 0) {
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        }
+        // Otherwise update the TextView to tell the user there is no connection or no search term.
+        else {
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
+        }
+    }
+}

f5b0d1ed66db09b51a992607f9634e1cef974060
lhashing <jamyangh@gmail.com>
2021-05-31 20:38:26 +0600

    ITW202_TODO_25

---
+++ TODO_25/app/src/main/java/edu/gcit/todo_25/DatabaseHelper.java
@@ -0,0 +1,48 @@
+package edu.gcit.todo_25;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "Student.db";
+    public static final String TABLE_NAME = "student_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "FIRSTNAME";
+    public static final String COL_3 = "LASTNAME";
+    public static final String COL_4 = "MARKS";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(ID INTEGER PRIMARY KEY, FIRSTNAME TEXT, LASTNAME TEXT, MARKS INTEGER)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String id, String firstname, String lastname, String marks) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, firstname);
+        contentValues.put(COL_3, lastname);
+        contentValues.put(COL_4, marks);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
---
+++ TODO_25/app/src/main/java/edu/gcit/todo_25/MainActivity.java
@@ -0,0 +1,54 @@
+package edu.gcit.todo_25;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+    private EditText Fname, Lname, Marks, id;
+    private Button addData, viewAll, update, delete;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myDb = new DatabaseHelper(this);
+
+        Fname = findViewById(R.id.firstName);
+        Lname = findViewById(R.id.lastName);
+        Marks = findViewById(R.id.marks);
+        id = findViewById(R.id.idNumber);
+
+        addData = findViewById(R.id.addData);
+        viewAll = findViewById(R.id.viewAll);
+        update = findViewById(R.id.update);
+        delete = findViewById(R.id.delete);
+        AddData();
+    }
+
+    public void AddData() {
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isInserted = myDb.insertData(id.getText().toString(),
+                        Fname.getText().toString(),
+                        Lname.getText().toString(),
+                        Marks.getText().toString());
+
+                if(isInserted == true) {
+                    Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Data Not Inserted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
